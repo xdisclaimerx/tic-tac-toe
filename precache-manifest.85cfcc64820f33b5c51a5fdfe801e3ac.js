8bbe30c4d7c46bb3b186642e15608848f649f22d
Amaro <christian.amaro@teletech.com>
2019-06-07 03:35:23 +1000

    Updates

---
+++ precache-manifest.85cfcc64820f33b5c51a5fdfe801e3ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1742a01f2eda53788bc313df180d3653",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e65e21d03f6f89ef38ab",
+    "url": "/tic-tac-toe/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "685d0c597e8419129599",
+    "url": "/tic-tac-toe/static/js/2.0e3f6a1a.chunk.js"
+  },
+  {
+    "revision": "e65e21d03f6f89ef38ab",
+    "url": "/tic-tac-toe/static/js/main.ad78be3a.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
